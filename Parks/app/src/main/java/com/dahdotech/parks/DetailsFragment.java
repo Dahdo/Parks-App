7092327dc9dd2e169e5e4728b1a2aaae03b6a9e7
Dahdo <dndashimye15@gmail.com>
2022-09-30 01:51:59 +0200

    setup ViewPager and pullup images

--- Parks/app/src/main/java/com/dahdotech/parks/DetailsFragment.java
+++ Parks/app/src/main/java/com/dahdotech/parks/DetailsFragment.java
@@ -2,14 +2,24 @@ package com.dahdotech.parks;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class DetailsFragment extends Fragment {
+import com.dahdotech.parks.adapter.ViewPagerAdapter;
+import com.dahdotech.parks.databinding.FragmentParksBinding;
+import com.dahdotech.parks.model.ParkViewModel;
 
+public class DetailsFragment extends Fragment {
+    private ParkViewModel parkViewModel;
+    private ViewPagerAdapter viewPagerAdapter;
+    private ViewPager2 viewPager;
     public DetailsFragment() {
         // Required empty public constructor
     }
@@ -27,8 +37,22 @@ public class DetailsFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        viewPager = view.findViewById(R.id.details_view_pager);
+
+        parkViewModel.getSelectedPark().observe(getViewLifecycleOwner(), park -> {
+            viewPagerAdapter = new ViewPagerAdapter(park.getImages());
+            viewPager.setAdapter(viewPagerAdapter);
+        });
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        parkViewModel = new ViewModelProvider(requireActivity())
+                .get(ParkViewModel.class);
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_details, container, false);
     }
